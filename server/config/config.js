57734fb9527cc22d88e17f10a2d6be644ab70fb3
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2018-04-28 02:53:19 +0100

    set up sequelize

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+import {} from 'dotenv/config'
+
+const envs = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.TEST_USER,
+    password: process.env.TEST_PASS,
+    database: process.env.TEST_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PD_USER,
+    password: process.env.PD_PASS,
+    database: process.env.PD_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+}
+
+export default envs
