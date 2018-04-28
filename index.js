feb114a86cd37358690d84d55dd9f41b48f2b912
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2018-04-28 02:16:19 +0100

    server setup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import {} from 'dotenv/config'
 
 const app = express()
+const router = express.Router()
+
+//populate the req.body property with the parsed body
+app.use(bodyParser())
+
 
 // expose the server to supertest
-app.listen(9000, () => {
+app.listen(process.env.PORT || 9000, () => {
   console.log('app is listening at port 9000')
 })
 
-export default app;
+export default app
