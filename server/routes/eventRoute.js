eed9d5c2a6537b30c40d84f5fcac7475538cc10c
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2018-04-29 21:59:13 +0100

    event route

---
+++ server/routes/eventRoute.js
@@ -0,0 +1,16 @@
+import EventController from '../controllers/eventController'
+
+const event = new EventController()
+
+const eventRoute = router => {
+  router.route('/api/v1/events')
+    .get(event.findAllEvents)
+    .post(event.createEvent)
+
+  router.route('/api/v1/events/:id')
+    .get(event.findEvent)
+    .put(event.updateEvent)
+    .delete(event.deleteEvent)
+}
+
+export default eventRoute
